c4dbdfc34e23b52a728f5fef3003499ce5296fa1
Peter Huynh <huynhxpeter@gmail.com>
2018-10-30 00:54:15 -0400

    insert new node in a binary search tree done

--- src/data-structures/tree/binary-search-tree/BinarySearchTreePractice.js
+++ src/data-structures/tree/binary-search-tree/BinarySearchTreePractice.js
@@ -34,8 +34,30 @@ class BinarySearchTree {
   // method to insert a node in a tree
   // it moves over the tree to find the location
   // to insert a node with with the data being passed in
-  insertNode(data, newNode) {
-    
+  // we will pass in the root (parent) node and compare it to the newNode
+  insertNode(node, newNode) {
+    // if new node is less than parent then move it to the left
+    if (newNode.data < node.data) {
+      // if left node is null then insert it here
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        // In computer programming, the term recursive describes a function or method that repeatedly calculates a smaller part of itself to arrive at the final result. It is similar to iteration, but instead of repeating a set of operations, a recursive function accomplishes repetition by referring to itself in its own definition. While the concept of recursive programming can be difficult to grasp initially, mastering it can be very useful. Recursion is one of the fundamental tools of computer science.
+
+        // if left node is not null then recursively find the next
+        // null left node to insert the newNode
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      // if data is more on the right then insert here
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        // if right node is not null then recursively find the next
+        // null right node to insert the newNode
+        this.insertNode(node.right, newNode);
+      }
+    }
   }
 
   // remove(data)
